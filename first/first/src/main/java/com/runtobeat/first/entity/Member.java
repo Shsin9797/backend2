b507a0bc9729abccd90688ac7e262f4dc094dc15
CHAEGODA <121277243+CHAEGODA@users.noreply.github.com>
2024-07-22 14:01:09 +0900

    Member Entity

---
+++ first/first/src/main/java/com/runtobeat/first/entity/Member.java
@@ -0,0 +1,9 @@
+package com.runtobeat.first.entity;
+
+public class Member {
+    String memberId;
+    String memberName;
+    Double totalDistance; // 타입확인
+    Double totalTime; // 타입확인
+    Double avgPace;
+}
