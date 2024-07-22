a720e4667435d9291f38f5630828a614bedbc1fc
CHAEGODA <121277243+CHAEGODA@users.noreply.github.com>
2024-07-22 14:25:42 +0900

    DailyRecord Entity

---
+++ first/first/src/main/java/com/runtobeat/first/entity/DailyRecord.java
@@ -0,0 +1,11 @@
+package com.runtobeat.first.entity;
+
+import java.time.LocalDate;
+
+public class DailyRecord {
+    String dailyRecordId;
+    Double dailyTotalDistance;
+    Double dailyTotalTime;
+    LocalDate yearMonthDate;
+    Double dailyRecordPace;
+}
