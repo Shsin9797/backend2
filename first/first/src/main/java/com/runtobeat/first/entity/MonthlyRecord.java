1fc0ee64cafe95c74054c1f89e1df702eb62443f
CHAEGODA <121277243+CHAEGODA@users.noreply.github.com>
2024-07-22 14:25:33 +0900

    MonthlyRecord Entity

---
+++ first/first/src/main/java/com/runtobeat/first/entity/MonthlyRecord.java
@@ -0,0 +1,13 @@
+package com.runtobeat.first.entity;
+
+import java.time.LocalDate;
+
+public class MonthlyRecord {
+    String monthlyRecordId;
+    String memberId;
+    Long monthlyTotalDistance;
+    Long monthlyTotalTime;
+    LocalDate yearMonth;
+    Double monthlyRecordPace;
+
+}
