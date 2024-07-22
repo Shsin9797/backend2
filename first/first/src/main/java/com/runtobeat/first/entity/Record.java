06d35e775a6e631d2524a6d9e9fdb9e2bd2fc099
CHAEGODA <121277243+CHAEGODA@users.noreply.github.com>
2024-07-22 14:25:04 +0900

    Record Entity

---
+++ first/first/src/main/java/com/runtobeat/first/entity/Record.java
@@ -0,0 +1,16 @@
+package com.runtobeat.first.entity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Record {
+    String recordId;
+    String memberId;
+    Long runningDistance;
+    LocalTime runningTime;
+    Long runningStep;
+    LocalDate recordDate;
+    Double recordPace;
+
+
+}
