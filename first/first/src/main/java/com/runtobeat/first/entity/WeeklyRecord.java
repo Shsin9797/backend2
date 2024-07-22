631f9bc0495fd97bcd8ce0844a74eef0696574aa
CHAEGODA <121277243+CHAEGODA@users.noreply.github.com>
2024-07-22 14:25:23 +0900

    WeeklyRecord Entity

---
+++ first/first/src/main/java/com/runtobeat/first/entity/WeeklyRecord.java
@@ -0,0 +1,13 @@
+package com.runtobeat.first.entity;
+
+import java.time.LocalDate;
+
+public class WeeklyRecord {
+    String weeklyRecordId;
+    String memberId;
+    Long weeklyTotalDistance;
+    Long weeklyTotalTime;
+    LocalDate yearWeek;
+    Double weeklyRecordPace;
+
+}
