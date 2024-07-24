e37866f43a2eb6e3b5a5abb43db6acb4dce06e07
CHAEGODA <121277243+CHAEGODA@users.noreply.github.com>
2024-07-24 14:46:21 +0900

    chore : DailyRecord 수정

--- first/first/src/main/java/com/runtobeat/first/repository/DailyRecordRepository.java
+++ first/first/src/main/java/com/runtobeat/first/repository/DailyRecordRepository.java
@@ -1,6 +1,8 @@
 package com.runtobeat.first.repository;
 
+import com.runtobeat.first.entity.DailyRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface DailyRecordRepository extends JpaRepository<Record, String> {
+public interface DailyRecordRepository extends JpaRepository<DailyRecord, String> {
 }
