3629c53a001a8d00b9582156648410f03b5309ea
CHAEGODA <121277243+CHAEGODA@users.noreply.github.com>
2024-07-24 14:47:32 +0900

    chore : Member 수정

--- first/first/src/main/java/com/runtobeat/first/dto/MemberResponseDTO.java
+++ first/first/src/main/java/com/runtobeat/first/dto/MemberResponseDTO.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
-@Setter
 public class MemberResponseDTO {
     private String memberId;
     private String memberName;
--- first/first/src/main/java/com/runtobeat/first/entity/Member.java
+++ first/first/src/main/java/com/runtobeat/first/entity/Member.java
@@ -2,11 +2,15 @@ package com.runtobeat.first.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Table(name ="Member")
 public class Member {
     @Id
