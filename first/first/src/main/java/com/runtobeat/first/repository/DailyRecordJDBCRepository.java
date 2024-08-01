fb494cc3c09d9121135c5b96f8951ff4538a757e
CHAEGODA <121277243+CHAEGODA@users.noreply.github.com>
2024-08-01 12:19:07 +0900

    fix : 주간 기록 update 수정

--- first/first/src/main/java/com/runtobeat/first/repository/DailyRecordJDBCRepository.java
+++ first/first/src/main/java/com/runtobeat/first/repository/DailyRecordJDBCRepository.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 
 @Repository
 public class DailyRecordJDBCRepository {
--- first/first/src/main/java/com/runtobeat/first/repository/WeeklyRecordRepository.java
+++ first/first/src/main/java/com/runtobeat/first/repository/WeeklyRecordRepository.java
@@ -1,6 +1,7 @@
 package com.runtobeat.first.repository;
 
 import com.runtobeat.first.entity.DailyRecord;
+import com.runtobeat.first.entity.Member;
 import com.runtobeat.first.entity.WeeklyRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,4 +11,6 @@ public interface WeeklyRecordRepository extends JpaRepository<WeeklyRecord, Long
     WeeklyRecord getByWeekYear(String weekYear);
 
     List<WeeklyRecord> findAllByMemberMemberId(Long memberId);
+
+    WeeklyRecord findByMemberAndYearWeek(Member member, String weekYear);
 }
